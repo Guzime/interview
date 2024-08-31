764dcbe3bbca9f48036ae778e5e6cbedb2dbf189
ntolmachev <i@ntolmachev.ru>
2024-08-31 17:09:20 +0300

    add gradle and change project structure

---
+++ src/main/java/solid/Account.java
@@ -0,0 +1,34 @@
+package solid;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private final String numberAccount;
+    private BigDecimal balance;
+
+    public Account(String numberAccount, BigDecimal balance) {
+        this.balance = balance;
+        this.numberAccount = numberAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "number account = " + numberAccount
+                + "\n balance = " + balance;
+    }
+
+    public BigDecimal getBalance() {
+        System.out.println("You request balance on account " + numberAccount);
+        return balance;
+    }
+
+    public void refill(BigDecimal sum) {
+        System.out.println("You refill balance on account " + numberAccount + " for sum " + sum);
+        balance = balance.add(sum);
+    }
+
+    public void payment(BigDecimal sum) {
+        System.out.println("You payment on account " + numberAccount + " for sum " + sum);
+        balance = balance.min(sum);
+    }
+}
