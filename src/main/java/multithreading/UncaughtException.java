0d81088b499dbbff9dc08b8e23c7b05c1468c9c2
ntolmachev <i@ntolmachev.ru>
2024-10-06 23:55:13 +0300

    Add create logic from threadFactory

--- src/main/java/multithreading/UncaughtException.java
+++ src/main/java/multithreading/UncaughtException.java
@@ -1,6 +1,7 @@
 package multithreading;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.ThreadFactory;
 
 import static java.lang.Thread.currentThread;
 
@@ -9,14 +10,30 @@ public class UncaughtException {
 
         UncaughtExceptionHandler handler = (thread, exception) -> {
             System.out.println("Catch exception: \n" + exception.getMessage()
-                    +"\nfrom thread " + thread.getName());
+                    + "\nfrom thread " + thread.getName());
         };
 
-        Thread thread = new Thread(()-> {
-           throw new RuntimeException("Throw uncaught exception from " + currentThread().getName());
+        Thread thread = new Thread(() -> {
+            throw new RuntimeException("Throw uncaught exception from " + currentThread().getName());
         });
         //По дефолту никакие исключения не ловятся, поток просто заканчивает свое существование
         thread.setUncaughtExceptionHandler(handler);
         thread.start();
+
+        ThreadFactory threadFactory = runnable -> {
+            Thread threadFromFactory = new Thread(runnable);
+            threadFromFactory.setDaemon(true);
+            threadFromFactory.setUncaughtExceptionHandler(handler);
+            return threadFromFactory;
+        };
+
+        Thread thread1 = threadFactory.newThread(() -> {
+            System.out.println(currentThread().getName()
+                    + " is daemon? - " + currentThread().isDaemon()
+                    + "\nwas created from factory");
+        });
+
+        thread1.start();
+
     }
 }
